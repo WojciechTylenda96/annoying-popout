48ab1987f2d4a925d3b2a72d3e089b121c9ca7dc
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2025-01-08 20:02:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 const modalEl = document.getElementById("modal")
+const modalCloseBtn = document.getElementById("modal-close-btn")
 
 setTimeout(function(){
     modalEl.style.display = "inline"
 }, 1500)
+
+modalCloseBtn.addEventListener("click", function(){
+    modalEl.style.display = "none"
+})
