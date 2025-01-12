9e194be5262c067362018ac43f14cf357fbf60ed
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2025-01-12 12:25:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-const modalEl = document.getElementById("modal")
-const modalCloseBtn = document.getElementById("modal-close-btn")
+const modalEl = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
 
 setTimeout(function(){
     modalEl.style.display = "inline"
-}, 1500)
+}, 1500);
 
 modalCloseBtn.addEventListener("click", function(){
     modalEl.style.display = "none"
-})
+});
+
+consentForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    console.log("form submitted")
+});
